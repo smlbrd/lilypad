1976df4b021914731b81b4b8bc51a6a9495cc18d
smlbrd <smallbirdquickthorn+github@gmail.com>
2025-04-03 11:22:30 +0100

    added second player container, rotated counter for far player

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,31 @@
-import { Text, View } from "react-native";
+import CreditsCounter from '@/components/CreditsCounter';
+import { StyleSheet, View } from 'react-native';
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.homeContainer}>
+      <View style={[styles.playerContainer, styles.farContainer]}>
+        <CreditsCounter />
+      </View>
+      <View style={styles.playerContainer}>
+        <CreditsCounter />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  homeContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000000',
+  },
+  playerContainer: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  farContainer: {
+    transform: [{ rotate: '180deg' }],
+  },
+});
